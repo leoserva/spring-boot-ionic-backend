d06338ff3a39369152e3678ee5efffb58dc8a04c
Leonardo Serva <leoserva@gmail.com>
2019-05-16 00:11:43 -0300

    Atualização: utilizando apenas JsonIgnore

--- src/main/java/com/leoserva/cursomc/domain/ItemPedido.java
+++ src/main/java/com/leoserva/cursomc/domain/ItemPedido.java
@@ -72,8 +72,6 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 
-
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -81,8 +79,6 @@ public class ItemPedido implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -101,5 +97,4 @@ public class ItemPedido implements Serializable {
 		return true;
 	}
 
-	
 }
